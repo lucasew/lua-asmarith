5270621a51755c6538ec0713f6cfd4bd69a15e6b
Lucas Eduardo <lucas59356@gmail.com>
2020-03-11 16:52:05 -0300

    Parece que por padrão o nasm compila em 32 bits

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Testado no arch linux, compilado pelo clang pois o mesmo tem o parâmetro target, o qual possibilita mais facilmente realizar a compilação 32 bits (i386)
 # O mesmo contém uma pequena suíte de testes para testar a binding no lado C e no lado Lua
 
-NASMFLAGS="-felf -DELF_TYPE"
+NASMFLAGS="-felf64 -DELF_TYPE"
 CFLAGS="-g -lc"
 
 build() {
