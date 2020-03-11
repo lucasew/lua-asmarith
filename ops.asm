1503b735f565916008967dc3191d053ca080c027
Lucas Eduardo <lucas59356@gmail.com>
2020-03-11 18:10:52 -0300

    Parece que deu bom. Aplicando nos outros

--- ops.asm
+++ ops.asm
@@ -14,16 +14,16 @@ func_sum:
 func_sub:
     push rbp
     mov rbp, rsp
-    mov rax, [rbp + 8]
-    sub rax, [rbp + 12]
+    mov rax, rsp
+    sub rax, rdi
     pop rbp
     ret
 
 func_mul:
     push rbp
     mov rbp, rsp
-    mov rax, [rbp + 8]
-    mul DWORD [rbp + 12]
+    mov rax, rsp
+    mul DWORD rdi
     pop rbp
     ret
 
@@ -31,8 +31,8 @@ func_div:
     push rbp
     mov rbp, rsp
     xor rdx, rdx ; Ainda bem que o eduardo me passou essa manha
-    mov rax, [rbp + 8]
-    div DWORD [rbp + 12]
+    mov rax, rsp
+    div DWORD rdi
     pop rbp
     ret
 
