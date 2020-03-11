e90dcc99b8cbeda70a8996f7eb66cb0c5b448bce
Lucas Eduardo <lucas59356@gmail.com>
2020-03-11 16:43:12 -0300

    Update ops.asm

--- ops.asm
+++ ops.asm
@@ -6,24 +6,24 @@ global func_sum, func_sub, func_mul, func_div
 func_sum:
     push rbp
     mov rbp, esp
-    mov rax, [rbp + 8]
-    add rax, [rbp + 12]
+    mov rax, [ebp + 8]
+    add rax, [ebp + 12]
     pop rbp
     ret
 
 func_sub:
     push rbp
     mov rbp, esp
-    mov rax, [rbp + 8]
-    sub rax, [rbp + 12]
+    mov rax, [ebp + 8]
+    sub rax, [ebp + 12]
     pop rbp
     ret
 
 func_mul:
     push rbp
     mov rbp, esp
-    mov rax, [rbp + 8]
-    mul DWORD [rbp + 12]
+    mov rax, [ebp + 8]
+    mul DWORD [ebp + 12]
     pop rbp
     ret
 
@@ -31,8 +31,8 @@ func_div:
     push rbp
     mov rbp, rsp
     xor rdx, rdx ; Ainda bem que o eduardo me passou essa manha
-    mov rax, [rbp + 8]
-    div DWORD [rbp + 12]
+    mov rax, [ebp + 8]
+    div DWORD [ebp + 12]
     pop rbp
     ret
 
