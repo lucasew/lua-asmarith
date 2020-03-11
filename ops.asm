8b64cd8e5cc4f3c267eb09947729600bc320d378
Lucas Eduardo <lucas59356@gmail.com>
2020-03-11 16:40:26 -0300

    Update ops.asm

--- ops.asm
+++ ops.asm
@@ -4,35 +4,35 @@ segment .text
 global func_sum, func_sub, func_mul, func_div
 
 func_sum:
-    push ebp
-    mov ebp, esp
-    mov eax, [ebp + 8]
-    add eax, [ebp + 12]
-    pop ebp
+    push rbp
+    mov rbp, esp
+    mov rax, [rbp + 8]
+    add rax, [rbp + 12]
+    pop rbp
     ret
 
 func_sub:
-    push ebp
-    mov ebp, esp
-    mov eax, [ebp + 8]
-    sub eax, [ebp + 12]
-    pop ebp
+    push rbp
+    mov rbp, esp
+    mov rax, [rbp + 8]
+    sub rax, [rbp + 12]
+    pop rbp
     ret
 
 func_mul:
-    push ebp
-    mov ebp, esp
-    mov eax, [ebp + 8]
-    mul DWORD [ebp + 12]
-    pop ebp
+    push rbp
+    mov rbp, esp
+    mov rax, [rbp + 8]
+    mul DWORD [rbp + 12]
+    pop rbp
     ret
 
 func_div:
-    push ebp
-    mov ebp, esp
-    xor edx, edx ; Ainda bem que o eduardo me passou essa manha
-    mov eax, [ebp + 8]
-    div DWORD [ebp + 12]
-    pop ebp
+    push rbp
+    mov rbp, rsp
+    xor rdx, rdx ; Ainda bem que o eduardo me passou essa manha
+    mov rax, [rbp + 8]
+    div DWORD [rbp + 12]
+    pop rbp
     ret
 
